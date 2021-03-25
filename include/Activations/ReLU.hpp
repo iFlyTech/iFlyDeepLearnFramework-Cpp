56341b18c46541c3698134e7c98a37cc781a803a
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2021-03-25 16:31:50 +0000

    Added "sr" and "sr@Latn" to ALL_LINGUAS.

--- include/Activations/ReLU.hpp
+++ include/Activations/ReLU.hpp
@@ -33,4 +33,4 @@ public:
    * Backward pass of the ReLU activation function.
    *
    * @param[out] ddout 1*input if forward input was > 0, else 0
-   * @p
+   * @param[in] dout Values on which to apply
