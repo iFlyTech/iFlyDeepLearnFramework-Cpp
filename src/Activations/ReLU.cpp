6a82e3d70ae2ab4c1ca5877e071bb1c81227ddef
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2021-06-28 10:41:13 +0000

    trunk merged

---
+++ src/Activations/ReLU.cpp
@@ -0,0 +1,23 @@
+
+/**
+ * ReLU layer class implementation
+ */
+
+#include "ReLU.hpp"
+
+#include <iostream>
+
+using namespace DeepLearningFramework::Activations;
+
+ReLU::ReLU() {}
+
+void ReLU::forward(Eigen::MatrixXf &out, const Eigen::MatrixXf &x) {
+  mForwardInput = x;
+  out = (x.array() < 0.f).select(0.f, x);
+}
+
+void ReLU::backward(Eigen::MatrixXf &ddout, const Eigen::MatrixXf &dout) {
+  ddout = (mForwardInput.array() < 0.f).select(0.f, dout);
+}
+
+void ReLU::printDescription() { std::cout << "ReLU activation" << std::endl; }
