61d09751d0596aa6df80d7c3ff8c18581b4a3e30
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2021-07-21 16:43:10 +0000

    Add GPLv3 license

--- src/Data/DataBuilder2D.cpp
+++ src/Data/DataBuilder2D.cpp
@@ -8,4 +8,4 @@ using namespace DeepLearningFramework;
 
 void DataBuilder2D::generateDiscSet(Eigen::MatrixXf &labels,
                                     Eigen::MatrixXf &features,
-                                
+                                    uint32_t samplesCount,
