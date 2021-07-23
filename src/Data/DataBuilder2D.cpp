c4fdb0f946fa57d5b909fb9b44b5fc35acb31a58
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2021-07-23 21:27:23 +0000

    subir

--- src/Data/DataBuilder2D.cpp
+++ src/Data/DataBuilder2D.cpp
@@ -9,4 +9,5 @@ using namespace DeepLearningFramework;
 void DataBuilder2D::generateDiscSet(Eigen::MatrixXf &labels,
                                     Eigen::MatrixXf &features,
                                     uint32_t samplesCount, float discRadius) {
-  features = Eigen::MatrixXf::Random(samplesCount,
+  features = Eigen::MatrixXf::Random(samplesCount, 2);
+  labels = Eigen::MatrixXf(samp
