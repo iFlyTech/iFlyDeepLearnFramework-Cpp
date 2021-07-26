e6d6d65b8796bce2fb8d28984c409fca20d4aaa7
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2021-07-26 13:05:05 +0000

    Release v0.0.8

--- src/Data/DataBuilder2D.cpp
+++ src/Data/DataBuilder2D.cpp
@@ -17,4 +17,5 @@ void DataBuilder2D::generateDiscSet(Eigen::MatrixXf &labels,
       labels(i, 0) = 0.f;
       labels(i, 1) = 1.f;
     } else {
-      labels
+      labels(i, 0) = 1.f;
+      labels(i, 1)
