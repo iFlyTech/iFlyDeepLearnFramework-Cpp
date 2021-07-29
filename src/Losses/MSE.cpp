046fb71255eaf33a55b011fa08e5919ffb915fba
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2021-07-29 17:39:08 +0000

    Linux 3.4.4

--- src/Losses/MSE.cpp
+++ src/Losses/MSE.cpp
@@ -11,4 +11,5 @@ using namespace DeepLearningFramework::Losses;
 MSE::MSE() {}
 
 void MSE::forward(float &loss, const Eigen::MatrixXf &y,
-                 
+                  const Eigen::MatrixXf &yPred) {
+ 
