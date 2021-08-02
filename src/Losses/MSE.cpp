0c458286e6eb00da16a2610c0aa95430c583e4ff
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2021-08-02 16:24:44 +0000

    Rename index.md to README.md

--- src/Losses/MSE.cpp
+++ src/Losses/MSE.cpp
@@ -16,4 +16,4 @@ void MSE::forward(float &loss, const Eigen::MatrixXf &y,
 }
 
 void MSE::backward(Eigen::MatrixXf &dloss, const Eigen::MatrixXf &y,
-         
+                   const Eigen::MatrixXf &y
