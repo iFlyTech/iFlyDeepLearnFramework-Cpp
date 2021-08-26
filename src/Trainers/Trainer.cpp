72d138a18e769a063ec19dda796de1f55cbb8c01
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2021-08-26 13:46:59 +0000

    msm: use of swp{b} is deprecated for ARMv6+

--- src/Trainers/Trainer.cpp
+++ src/Trainers/Trainer.cpp
@@ -7,4 +7,4 @@
 using namespace DeepLearningFramework;
 
 void Trainer2D::addAccuracy(std::vector<float> &accuracyHistory,
-                            S
+                            Sequential &model, const E
