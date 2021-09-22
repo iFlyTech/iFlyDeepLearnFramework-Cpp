637e04af678b977f8b57090d1f8e0a5ab6c69b9f
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2021-09-22 11:21:20 +0000

    Updating readme.txt

--- tests/main.cpp
+++ tests/main.cpp
@@ -14,3 +14,37 @@ int main() {
   /* Model creation */
   std::vector<Module *> layers;
   int inputFeaturesNumber = 2, outputFeaturesNumber = 2, hiddenSize = 10;
+  layers.emplace_back(
+      new Layers::Linear((int)inputFeaturesNumber, (int)hiddenSize));
+  layers.emplace_back(new Activations::ReLU());
+  layers.emplace_back(new Layers::Linear((int)hiddenSize, (int)hiddenSize));
+  layers.emplace_back(new Activations::ReLU());
+  layers.emplace_back(
+      new Layers::Linear((int)hiddenSize, (int)outputFeaturesNumber));
+  layers.emplace_back(new Activations::Softmax());
+
+  Losses::MSE mseLoss;
+
+  Sequential model(layers, mseLoss);
+  model.printDescription();
+
+  /* Train params */
+  float learningRate = 0.03f;
+  // number of train and test samples
+  uint32_t samplesCount = 2000;
+  std::vector<float> trainLossHistory, trainAccuracyHistory, testLossHistory,
+      testAccuracyHistory;
+  uint32_t epochsCount = 100, verboseFrequence = 1;
+  constexpr auto batchSize = 64;
+
+  // Update learning rate for model
+  model.setLR(learningRate);
+
+  /* Generate train and test sets */
+  Eigen::MatrixXf trainTarget, trainFeatures;
+  DataBuilder2D::generateDiscSet(trainTarget, trainFeatures, samplesCount, 0.3);
+  Eigen::MatrixXf testTarget, testFeatures;
+  DataBuilder2D::generateDiscSet(testTarget, testFeatures, samplesCount, 0.3);
+
+  // Train model
+  Trainer2D::trainModel<batchSize>(trainLossHistory, trainAccuracyHistory,
