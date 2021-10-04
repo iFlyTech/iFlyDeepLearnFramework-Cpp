587ebdcd245acefaa4fdc9cabc64a8c3eab82b02
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2021-10-04 09:37:58 +0000

    Copy over revisions from the trunk:

--- tests/unitTests/include/UnitTestsActivationsSoftmax.hpp
+++ tests/unitTests/include/UnitTestsActivationsSoftmax.hpp
@@ -11,4 +11,5 @@ public:
   static void softmaxActivationForwardPassTest();
   static void softmaxActivationBackwardPassTest();
 };
-}; // namespace
+}; // namespace Activations
+}; // namespace DeepLe
