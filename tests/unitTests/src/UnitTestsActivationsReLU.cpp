0a892e993f3c1b724469eb26a87f7b85c3c8af8c
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2021-10-27 11:58:23 +0000

    libceph: change from BUG to WARN for __remove_osd() asserts

--- tests/unitTests/src/UnitTestsActivationsReLU.cpp
+++ tests/unitTests/src/UnitTestsActivationsReLU.cpp
@@ -16,4 +16,4 @@ void Activations::UnitTestsActivationsReLU::reluActivationForwardPassTest() {
 
   Eigen::MatrixXf target{
       {0.f, 0.f, 0.f, 1.f, 2.f, 8.f},
-      
+      {9.f, 0.f, 0.f, 1.f, 0.f, 8.f},
