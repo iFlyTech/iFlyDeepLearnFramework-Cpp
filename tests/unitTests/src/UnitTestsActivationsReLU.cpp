0c9d03c5936ae1cd458e5c22d5a462898f504850
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2021-11-04 21:57:13 +0000

    update yarn

--- tests/unitTests/src/UnitTestsActivationsReLU.cpp
+++ tests/unitTests/src/UnitTestsActivationsReLU.cpp
@@ -43,4 +43,4 @@ void Activations::UnitTestsActivationsReLU::reluActivationBackwardPassTest() {
   Eigen::MatrixXf forwardX{
       {3.f, -5.f, 0.f, 1.f, 2.f, 7.f},
       {9.f, -5.f, 0.f, 1.f, -2.f, -8.f},
-   
+      {-4.f, 5.f, 8.f, -1.f, 2.f, 
