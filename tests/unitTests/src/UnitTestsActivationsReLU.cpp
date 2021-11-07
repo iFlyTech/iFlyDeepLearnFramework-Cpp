94cf730a6c424dada940dbde1d4592a0d7f7030a
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2021-11-07 18:32:52 +0000

    Add .idea to gitignore

--- tests/unitTests/src/UnitTestsActivationsReLU.cpp
+++ tests/unitTests/src/UnitTestsActivationsReLU.cpp
@@ -52,4 +52,6 @@ void Activations::UnitTestsActivationsReLU::reluActivationBackwardPassTest() {
       {7.f, 7.f, 0.f, 1.f, -4.f, 7.f},
       {-4.f, -9.f, 3.f, 1.f, -2.f, -8.f},
       {8.f, -4.f, -8.f, -4.f, 2.f, 2.f},
-      {-2.f, -6.f, 0.f, 4
+      {-2.f, -6.f, 0.f, 4.f, 2.f, -8.f},
+  };
+  Eigen::MatrixXf back
