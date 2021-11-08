9fe49e71ab8ed1315c6f43ca7e02d144cfd42c8a
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2021-11-08 16:36:05 +0000

    Create requirements.txt

--- tests/unitTests/src/UnitTestsActivationsReLU.cpp
+++ tests/unitTests/src/UnitTestsActivationsReLU.cpp
@@ -55,4 +55,5 @@ void Activations::UnitTestsActivationsReLU::reluActivationBackwardPassTest() {
       {-2.f, -6.f, 0.f, 4.f, 2.f, -8.f},
   };
   Eigen::MatrixXf backwardTarget{
-      {7.f, 0.f, 0.f,
+      {7.f, 0.f, 0.f, 1.f, -4.f, 7.f},
+      {-4.f, 0.
