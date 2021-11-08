3b466a35c3ca26f2de94fd648c448df1b9a6eadc
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2021-11-08 09:04:54 +0000

    Create Image_List.html

--- tests/unitTests/src/UnitTestsActivationsReLU.cpp
+++ tests/unitTests/src/UnitTestsActivationsReLU.cpp
@@ -54,4 +54,5 @@ void Activations::UnitTestsActivationsReLU::reluActivationBackwardPassTest() {
       {8.f, -4.f, -8.f, -4.f, 2.f, 2.f},
       {-2.f, -6.f, 0.f, 4.f, 2.f, -8.f},
   };
-  Eigen::MatrixXf back
+  Eigen::MatrixXf backwardTarget{
+      {7.f, 0.f, 0.f,
