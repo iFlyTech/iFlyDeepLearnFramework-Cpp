620a058559ae72254326d126452cd2a69a19f64a
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2022-01-21 09:17:53 +0000

    #150

--- tests/unitTests/src/UnitTestsLossesMSE.cpp
+++ tests/unitTests/src/UnitTestsLossesMSE.cpp
@@ -42,4 +42,7 @@ void Losses::UnitTestsLossesMSE::mseLossBackwardPassTest() {
   Eigen::MatrixXf y{
       {1.f, 0.f},
       {1.f, 0.f},
-      {0.f, 
+      {0.f, 1.f},
+  };
+
+  Eigen::MatrixXf yPr
