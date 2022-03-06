c02016baf79a3d18c0c841ff3c8a5a1e0105e57a
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2022-03-06 20:45:37 +0000

    Correct link

--- tests/unitTests/src/UnitTestsLossesMSE.cpp
+++ tests/unitTests/src/UnitTestsLossesMSE.cpp
@@ -62,4 +62,5 @@ void Losses::UnitTestsLossesMSE::mseLossBackwardPassTest() {
 
   if (!target.isApprox(out)) {
     std::cout << "Derivative KO" << std::endl;
-    std::cout << "Expect: " << target <<
+    std::cout << "Expect: " << target << std::endl;
+    std::cout << "Got:
